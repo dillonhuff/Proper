d4e3551569ac5277509d6514edd81be0ccb84fde
dh25929 <dh25929@wahlberg.(none)>
2014-06-13 20:32:53 -0500

    checking validity by truth table now working

--- src/Sentence.hs
+++ src/Sentence.hs
@@ -1,7 +1,8 @@
 module Sentence(
                neg, con, dis, val, bic, imp,
                truthAssignment,
-               evalSentence) where
+               evalSentence,
+               isValidByTruthTable) where
 
 import Data.Map as M
 
@@ -23,12 +24,25 @@ bic s1 s2 = Bic s1 s2
 imp s1 s2 = Imp s1 s2
 val name = Val name
 
+constants :: Sentence -> [Sentence]
+constants (Val n) = [(Val n)]
+constants (Neg s) = constants s
+constants (Con s1 s2) = constants s1 ++ constants s2
+constants (Dis s1 s2) = constants s1 ++ constants s2
+constants (Bic s1 s2) = constants s1 ++ constants s2
+constants (Imp s1 s2) = constants s1 ++ constants s2
+
 type TruthAssignment = Map Sentence Bool
 
+truthVal :: Sentence -> TruthAssignment -> Bool
+truthVal s tt = case M.lookup s tt of
+  Just val -> val
+  Nothing -> error $ "Sentence not in truth table " ++ show s
+
 truthAssignment :: [Name] -> [Bool] -> TruthAssignment
-truthAssignment constNames constVals = M.fromList $ zip constants constVals
+truthAssignment constNames constVals = M.fromList $ zip consts constVals
   where
-    constants = Prelude.map val constNames
+    consts = Prelude.map val constNames
 
 evalSentence :: TruthAssignment -> Sentence -> Bool
 evalSentence a (Neg s) = not $ evalSentence a s
@@ -42,6 +56,40 @@ evalSentence a (Bic s1 s2) = (s1Eval && s2Eval) || ((not s1Eval) && (not s2Eval)
   where
     s1Eval = evalSentence a s1
     s2Eval = evalSentence a s2
-evalSentence a c@(Val name) = case M.lookup c a of
-  Just boolVal -> boolVal
-  Nothing -> error $ "Constant with not truth value " ++ show c
+evalSentence a constant = truthVal constant a
+  
+type TruthTable = [TruthAssignment]
+
+containsSentence :: Sentence -> TruthTable -> Bool
+containsSentence s [] = False
+containsSentence s tt = M.member s (head tt)
+
+truthTable :: [Sentence] -> TruthTable
+truthTable sents = Prelude.foldl addSentence [] sents
+
+addSentence :: TruthTable -> Sentence -> TruthTable
+addSentence tt s = if (containsSentence s tt)
+  then tt
+  else addNewSentence tt s
+
+addNewSentence :: TruthTable -> Sentence -> TruthTable
+addNewSentence [] c@(Val n) = [truthAssignment [n] [True], truthAssignment [n] [False]]
+addNewSentence tt c@(Val n) = ttFalse ++ ttTrue
+ where
+   ttFalse = Prelude.map (\ta -> M.insert c False ta) tt
+   ttTrue = Prelude.map (\ta -> M.insert c True ta) tt
+addNewSentence tt s = Prelude.map (addCompoundSentence s) tt
+
+addCompoundSentence :: Sentence -> TruthAssignment -> TruthAssignment
+addCompoundSentence s ta = insert s sval ta
+  where
+    sval = evalSentence ta s
+
+truthTableForSentence :: Sentence -> TruthTable
+truthTableForSentence s = truthTable $ (constants s) ++ [s]
+                          
+isValidByTruthTable :: Sentence -> Bool
+isValidByTruthTable s = and sTruthVals
+  where
+    sTruthTable = truthTableForSentence s
+    sTruthVals = Prelude.map (truthVal s) sTruthTable
--- test/SentenceTests.hs
+++ test/SentenceTests.hs
@@ -5,13 +5,14 @@ import TestUtils
 
 allSentenceTests = do
   testSimpleEval
+  testIsValidByTruthTable
 
 testSimpleEval =
-  testFunction (evalSentence simpleTA) simpleCases
+  testFunction (evalSentence simpleTA) evalCases
   
 simpleTA = truthAssignment ["p", "q", "r", "s"] [True, False, True, True]
 
-simpleCases =
+evalCases =
   [(neg (val "p"), False),
    (neg (val "q"), True),
    (con (val "p") (val "q"), False),
@@ -23,3 +24,17 @@ simpleCases =
    (bic (val "q") (val "s"), False),
    (imp (val "q") (val "s"), True),
    (imp (val "p") (val "q"), False)]
+  
+testIsValidByTruthTable =
+  testFunction isValidByTruthTable isValidTTCases
+  
+isValidTTCases =
+  [(val "a", False),
+   (dis (val "a") (neg (val "a")), True),
+   (dis (val "a") (val "a"), False),
+   (imp (con (val "a") (val "b")) (val "a"), True),
+   (imp (val "q") (con (val "a") (val "q")), False),
+   (bic (neg (neg (val "b"))) (val "b"), True),
+   (imp (con (val "p") (val "q")) (val "p"), True),
+   (imp (dis (val "p") (val "q")) (val "p"), False),
+   (bic (neg (con (val "p") (val "q"))) (dis (neg (val "p")) (neg (val "q"))), True)]
