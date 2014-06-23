b91793bcf3178e51f4a01cb6f5b3b01815a6c851
dh25929 <dh25929@wahlberg.(none)>
2014-06-22 19:41:54 -0500

    added a few more tests of naiveSAT

--- test/Proper/CNFTests.hs
+++ test/Proper/CNFTests.hs
@@ -18,11 +18,18 @@ satCases =
    (cnf $ [clause [nLit "a", lit "p"]], True),
    (cnf $ [clause [nLit "p"],
            clause [lit "p", nLit "s"],
-           clause [lit "p"]], False)]
+           clause [lit "p"]], False),
+   (toCNF (imp (val "a") (val "a")), True),
+   (toCNF (bic (val "c") (val "d")), True),
+   (toCNF (neg (bic (val "c") (val "c"))), False)]
   
 testNaiveSATComplicated =
   testFunction naiveSAT complicatedCases
   
 complicatedCases =
   [(toCNF (neg (bic (neg (con (val "p") (val "q"))) (dis (neg (val "p")) (neg (val "q"))))), False),
-   (toCNF (bic (neg (dis (val "p") (val "q"))) (con (neg (val "p")) (neg (val "q")))), True)]
+   (toCNF (bic (neg (dis (val "p") (val "q"))) (con (neg (val "p")) (neg (val "q")))), True),
+   (toCNF
+    (neg (bic
+          (neg (dis (con (val "a") (neg (val "c"))) (dis (val "d") (val "f"))))
+          (con (dis (neg (val "a")) (val "c")) (con (neg (val "d")) (neg (val "f")))))), False)]
